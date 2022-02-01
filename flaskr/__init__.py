f1408be5edd1c173e34bafdc4843593e0cf0cb13
FrancescoLuzzi <francescoluzzi99@libero.it>
2022-02-01 22:48:24 +0100

    added pose detection and face cover for images

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,4 +32,35 @@ def create_app(test_config=None):
     def hello():
         return render_template("basePage.html", title="FaceUnderCover")
 
+    @app.route("/facedetection", methods=["POST"])
+    def i_cant_see_you():
+        global model_factory
+        global painter_factory
+        model = model_factory.aquire_model()
+        painter = painter_factory.aquire_painter()
+        model.init_net()
+
+        file = request.files["file"]
+
+        if "image" in file.mimetype:
+            img = cv.imdecode(np.frombuffer(file.read(), np.uint8), cv.IMREAD_UNCHANGED)
+            height, width, _ = np.shape(img)
+            detections = model.find_detections(
+                frame=img, frame_width=width, frame_height=height
+            )
+            painter.paint_frame(img, detections=detections)
+            output = cv.imencode(file.mimetype.replace("image/", "."), img)[1].tobytes()
+            response = make_response(output)
+            response.headers.set("Content-Type", file.mimetype)
+            response.headers.set(
+                "Content-Disposition", "attachment", filename=f"OUT_{file.filename}"
+            )
+            return response
+        elif "video" in file.mimetype:
+            print("Video")
+        else:
+            print("ERROR")
+
+        return "OKOK"
+
     return app
