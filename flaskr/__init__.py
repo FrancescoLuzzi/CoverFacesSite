6ede76111d178f90ccf29630f2d01c57fb3f091e
FrancescoLuzzi <francescoluzzi99@libero.it>
2022-02-06 19:45:17 +0100

    refactoring

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ import os
 import cv2 as cv
 import numpy as np
 from flask import Flask, render_template, request, make_response
+from werkzeug.datastructures import FileStorage
 
 from flaskr.flask_cv_factories import factories
 
@@ -32,34 +33,39 @@ def create_app(test_config=None):
     def hello():
         return render_template("basePage.html", title="FaceUnderCover")
 
+    def process_image(file: FileStorage, painter, model):
+        img = cv.imdecode(np.frombuffer(file.read(), np.uint8), cv.IMREAD_UNCHANGED)
+        height, width, _ = np.shape(img)
+        detections = model.find_detections(
+            frame=img, frame_width=width, frame_height=height
+        )
+        painter.paint_frame(img, detections=detections)
+        output = cv.imencode(file.mimetype.replace("image/", "."), img)[1].tobytes()
+        response = make_response(output)
+        response.headers.set("Content-Type", file.mimetype)
+        response.headers.set(
+            "Content-Disposition", "attachment", filename=f"OUT_{file.filename}"
+        )
+        return response
+
     @app.route("/facedetection", methods=["POST"])
     def i_cant_see_you():
         global model_factory
         global painter_factory
         model = model_factory.aquire_model()
         painter = painter_factory.aquire_painter()
-        model.init_net()
 
         file = request.files["file"]
 
         if "image" in file.mimetype:
-            img = cv.imdecode(np.frombuffer(file.read(), np.uint8), cv.IMREAD_UNCHANGED)
-            height, width, _ = np.shape(img)
-            detections = model.find_detections(
-                frame=img, frame_width=width, frame_height=height
-            )
-            painter.paint_frame(img, detections=detections)
-            output = cv.imencode(file.mimetype.replace("image/", "."), img)[1].tobytes()
-            response = make_response(output)
-            response.headers.set("Content-Type", file.mimetype)
-            response.headers.set(
-                "Content-Disposition", "attachment", filename=f"OUT_{file.filename}"
-            )
-            return response
+            return process_image(file, painter, model)
         elif "video" in file.mimetype:
             print("Video")
         else:
             print("ERROR")
+
+        model_factory.release_model(model)
+        painter_factory.release_painter(painter)
 
         return "OKOK"
 
--- flaskr/flask_cv_factories/factories.py
+++ flaskr/flask_cv_factories/factories.py
@@ -2,13 +2,13 @@ import flaskr.Tesi.components as components
 
 
 class modelFactory:
-    free_models: "list[components.models.Model]" = []
-    models_in_use: "list[components.models.Model]" = []
-    currently_in_use: int = 0
-    max_size: int = 5
+    __free_models: "list[components.models.Model]" = []
+    __models_in_use: "list[components.models.Model]" = []
+    __currently_in_use: int = 0
+    __max_size: int = 5
 
     def __init__(self) -> None:
-        for _ in range(self.max_size):
+        for _ in range(self.__max_size):
             model = components.model_factory(
                 multiple=True,
                 gpu_enable=True,
@@ -16,50 +16,50 @@ class modelFactory:
                 proto_path="./flaskr/Tesi/mpi/pose_deploy_linevec_faster_4_stages.prototxt",
             )
             model.init_net()
-            self.free_models.append(model)
+            self.__free_models.append(model)
 
     def aquire_model(self):
-        if self.currently_in_use == self.max_size:
+        if self.__currently_in_use == self.__max_size:
             return None
         else:
-            self.currently_in_use += 1
-            model = self.free_models.pop()
-            self.models_in_use.append(model)
+            self.__currently_in_use += 1
+            model = self.__free_models.pop()
+            self.__models_in_use.append(model)
             return model
 
     def release_model(self, model: components.models.Model):
-        if self.currently_in_use == 0:
+        if self.__currently_in_use == 0:
             raise Exception
         else:
-            self.models_in_use.remove(model)
-            self.free_models.append(model)
-            self.currently_in_use -= 1
+            self.__models_in_use.remove(model)
+            self.__free_models.append(model)
+            self.__currently_in_use -= 1
 
 
 class painterFactory:
-    free_painters: "list[components.painters.Painter]" = []
-    painters_in_use: "list[components.painters.Painter]" = []
-    currently_in_use: int = 0
-    max_size: int = 5
+    __free_painters: "list[components.painters.Painter]" = []
+    __painters_in_use: "list[components.painters.Painter]" = []
+    __currently_in_use: int = 0
+    __max_size: int = 5
 
     def __init__(self) -> None:
-        for _ in range(self.max_size):
+        for _ in range(self.__max_size):
             painter = components.painter_factory(private=True)
-            self.free_painters.append(painter)
+            self.__free_painters.append(painter)
 
     def aquire_painter(self):
-        if self.currently_in_use == self.max_size:
+        if self.__currently_in_use == self.__max_size:
             return None
         else:
-            self.currently_in_use += 1
-            painter = self.free_painters.pop()
-            self.painters_in_use.append(painter)
+            self.__currently_in_use += 1
+            painter = self.__free_painters.pop()
+            self.__painters_in_use.append(painter)
             return painter
 
     def release_painter(self, model: components.painters.Painter):
-        if self.currently_in_use == 0:
+        if self.__currently_in_use == 0:
             raise Exception
         else:
-            self.painters_in_use.remove(model)
-            self.free_painters.append(model)
-            self.currently_in_use -= 1
+            self.__painters_in_use.remove(model)
+            self.__free_painters.append(model)
+            self.__currently_in_use -= 1
